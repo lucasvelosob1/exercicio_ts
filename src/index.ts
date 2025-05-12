e8b6644194d55054c922a29c6cf8c1e6a6e66557
Lucas <lucasvelosoebac@gmail.com>
2025-05-12 19:32:08 -0300

    exercicio typescript

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+//teste
+console.log(multiplicar(3, 4));
+console.log(saudacao("Lucas"));
